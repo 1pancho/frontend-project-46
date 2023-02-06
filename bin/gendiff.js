fee4d5eeae36017c00177024ed7be84d7dadd0c1
Timerlan Medov <medovcloud@me.com>
2023-02-06 13:35:20 +0700

    Second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,10 @@ const program = new Command();
 
 program
     .helpOption('-h, --help', 'display help for command')
-    .version('0.8.0');
-    
+    .version('0.8.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
     
