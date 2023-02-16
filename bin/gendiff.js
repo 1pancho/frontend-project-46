26618b23daef48728fe158384145a4e90a9e04ae
Timerlan Medov <medovcloud@me.com>
2023-02-16 13:13:13 +0700

    Third step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -8,10 +10,9 @@ program
     .version('0.8.0')
     .argument('<filepath1>')
     .argument('<filepath2>')
-    .option('-f, --format <type>', 'output format');
+    .option('-f, --format <type>', 'output format')
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(filepath1,filepath2));
+    });
 
-program.parse();
-    
-
-console.log('1');
-
+program.parse(process.argv);
