14631c270b033a4b83179ab5aee458a3ac077539
Timerlan Medov <medovcloud@me.com>
2023-04-13 14:11:57 +0700

    changes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,30 +3,29 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const getFixturePath = (filename) => path.join('..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedStylishFormat = genDiff('resultJsonFile.txt').trim();
+const expectedStylishFormat = readFile('resultJsonFile.txt').trim();
 
 test('check json stylish format', () => {
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
+  const actual = genDiff(getFixturePath('filepath1.json'), getFixturePath('filepath2.json'), 'stylish');
   expect(actual).toEqual(expectedStylishFormat);
 });
 
 test('check plain format', () => {
   const sourceData = genDiff('resultJsonFile.txt');
   const expected = sourceData.trim();
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
+  const actual = genDiff(getFixturePath('filepath.json'), getFixturePath('filepath.json'), 'stylish');
   expect(actual).toEqual(expected);
 });
 
 test('unknown format', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
+  const filepath1 = getFixturePath('filepath.json');
+  const filepath2 = getFixturePath('filepath.json');
   const error = new Error("This format is not supported: 'txt'. Please read the documentation and use the available formats");
   expect(() => {
     genDiff(filepath1, filepath2, 'txt');
--- src/index.js
+++ src/index.js
@@ -1,31 +1,59 @@
 /* eslint-disable no-restricted-syntax */
 import { readFileSync } from 'fs';
 import _ from 'lodash';
+ 
+const buildTree = (obj1, obj2) => {
+  const keysOfObj1 = _.keys(obj1);
+  const keysOfObj2 = _.keys(obj2);
+  const sortedKeys = _.sortBy(_.union(keysOfObj1, keysOfObj2));
+
+    const result = sortedKeys.map((key) => {
+      if (!_.has(obj1, key)) {
+        return {
+          key,
+          value: obj2[key],
+          type: 'added',
+        }; 
+      }
+      if (!_.has(obj2, key)) {
+        return {
+          key,
+          value: obj1[key],
+          type: 'deleted',
+        };
+      }
+      if(_.isObject(obj1[key]) && _.isObject(obj2[key])){
+        return {
+          key,
+          type: 'nested',
+          children: compareData(obj1[key], obj2[key]),
+        };
+      }
+      if (obj1[key] !== obj2[key]){
+        return {
+          key,
+          valueBefore: obj1[key],
+          valueAfter: obj2[key],
+          type: 'changed',
+        };
+      }
+      return {
+        key,
+        value: obj1[key],
+        type: 'unchanged',
+      };
+  });
+
+  return result;
+};
+
 
 const genDiff = (filepath1, filepath2) => {
   const dataParse1 = JSON.parse(readFileSync(filepath1, 'utf-8'));
   const dataParse2 = JSON.parse(readFileSync(filepath2, 'utf-8'));
   const genDiffFn = (dataParse1, dataParse2) => {
-    const result = {};
-    const keysOfObj1 = _.keys(dataParse1);
-    const keysOfObj2 = _.keys(dataParse2);
-    const keys = _.union(keysOfObj1, keysOfObj2);
-    for (const key of keys) {
-      if (!_.has(dataParse1, key)) {
-        result[key] = '+';
-      } else if (!_.has(dataParse2, key)) {
-        result[key] = '-';
-      } else if (dataParse1[key] !== dataParse2[key]) {
-        result[key] = '-';
-      } else {
-        result[key] = '';
-      }
-    }
-
-    return result;
-  };
-
-  return genDiffFn(dataParse1, dataParse2);
+    const result = buildTree(dataParse1, dataParse2) =;
+  }
 };
 
 export default genDiff;
