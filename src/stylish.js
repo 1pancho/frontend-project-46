a87aa90af4dc70b7a13cc062529cf574d71caefa
Timerlan Medov <medovcloud@me.com>
2023-06-14 09:24:36 +0300

    -6 step

--- src/stylish.js
+++ src/stylish.js
@@ -1,23 +1,19 @@
 
-const stringify = (value, replacer = ' ', spacesCount = 1) => {
-  const iter = (currentValue, depth) => {
-    if (!_.isObject(currentValue)) {
-      return `${currentValue}`;
-    }
+import _ from 'lodash';
 
-    const entries = Object.entries(currentValue);
-    const indent = replacer.repeat(spacesCount * depth);
-
-    const arrEl = entries.reduce((acc, [key, val]) => {
-      if (typeof val === 'object') {
-        val = iter(val, depth + 1);
-      }
-      return acc.concat(`${indent}${key}: ${val}\n`);
-    }, []);
-
-    return `{\n${arrEl.join('')}${replacer.repeat(spacesCount * depth - spacesCount)}}`;
-  };
-  return iter(value, 1);
+const stringify = (value, depth = 1) => {
+	if (!_.isObject(value)) {
+		return value;
+	}
+	const keys = Object.keys(value);
+	const getKeys = keys.map(
+		(key) =>
+			`${makeIndent(depth + 1)}  ${key}: ${stringify(
+				value[key],
+				depth + 1
+			)}`
+	);
+	return `{\n${getKeys.join("\n")}\n  ${makeIndent(depth)}}`;
 };
 
 
@@ -30,32 +26,33 @@ const stylish = (data) => {
     const iter = (tree, depth) => tree.map((node) => {
         switch (node.type) {
             case 'add':
-                return `${currentIndent(depth)}+ ${node.key}: ${stringify(
+                return `${makeIndent(depth)}+ ${node.key}: ${stringify(
                     node.value,
                     depth,
                 )}\n`;
             case 'deleted':
-              return `${currentIndent(depth)}- ${node.key}: ${stringify(
+              return `${makeIndent(depth)}- ${node.key}: ${stringify(
                   node.value,
                   depth,
                 )}\n`;
             case 'unchanged':
-              return `${currentIndent(depth)} ${node.key}: ${stringify(
+              return `${makeIndent(depth)} ${node.key}: ${stringify(
                 node.value,
                 depth,
               )}\n`;
             case 'changed':
-              return `${currentIndent(depth)}- ${node.key}: ${stringify(node.valueBefore, depth)}\n+ ${node.key}: ${stringify(node.valueAfter, depth)}\n`;
+              return `${makeIndent(depth)}- ${node.key}: ${stringify(node.valueBefore, depth)}\n+ ${node.key}: ${stringify(node.valueAfter, depth)}\n`;
             case 'nested':
-              return `${makeIndent(depth)}  ${node.key}: {\n${node.children
-                .map((val) => stylish(val, depth + 1))
-                .join("\n")}\n ${makeIndent(depth)} }`;
+              return `${makeIndent(depth)}  ${node.key}: {\n${iter(
+                node.children,
+                depth + 1,
+                ).join("\n")}\n ${makeIndent(depth)} }`;
             default:
               throw new Error(`Unknown type: ${node.type}`);
-          }
-        })
-        return `{\n${iter(data, 1).join('')}}`
-      }
+        }
+      })
+  return `{\n${iter(data, 1).join('')}}`
+}
                     
 //                 'add' формируется строка добавления свойства.
 // Для узла типа 'remove' формируется строка удаления свойства.
