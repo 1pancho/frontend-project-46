9e650db0a059248049c49fad0bb899ca853f8d44
Timerlan Medov <medovcloud@me.com>
2023-06-21 16:40:23 +0300

    added tests and README

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
 
 <a href="https://codeclimate.com/github/1pancho/frontend-project-46/test_coverage"><img src="https://api.codeclimate.com/v1/badges/b026ed49de397af0e60d/test_coverage" /></a>
 
+
+
+
+### Демонстрация запуска работы утилиты:
+### Сравнение без выбора формата (по умолчанию stylish)
+https://asciinema.org/a/BqS2LC6rDb8rnrB6BF8eUwOjT
+
+### Сравнение с форматом plain
+https://asciinema.org/a/Sp3PiRz4e0NawBiDzpIvTyYTR
+
+### Сравнение с форматом json
+https://asciinema.org/a/FsxB5iMRUQbmOGHMDSlknLMAP
+
--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,7 +10,7 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const expectedStylishFormat = readFile('resultStylish.txt');
 const expectedPlainFormat = readFile('resultPlain.txt').trim();
-const expectedJsonStylishFormat = JSON.stringify(readFile('resultStylish.txt'))
+const expectedJsonFormat = readFile('resultJson.txt');
 
 
 test('check stylish format', () => {
@@ -27,7 +27,7 @@ test('check plain format', () => {
 
 
 test('check json plain format', () => {
-  const actual = genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'json');
+  const actual = genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json');
   // console.log('actual', actual);
-  expect(actual).toEqual(expectedJsonStylishFormat);
+  expect(actual).toEqual(expectedJsonFormat);
 });
